24cdaed11edca14aad81a27a78b03ffba91735ab
Jack <mcnairjackr@gmail.com>
2018-04-25 10:42:22 +0800

    pthreadTest2 improved

--- pthreadTest2.c
+++ pthreadTest2.c
@@ -2,18 +2,39 @@
 #include <stdio.h>
 #include <pthread.h>
 
-void* sharedRead( int*, int );
-void* shoop();
-void* daWhoop();
-int shooping = 0;
+void* sharedRead( void* );
+void* shoop( void* );
+void* daWhoop( void* );
+int shooping = 1;
 pthread_mutex_t mutex1 = PTHREAD_MUTEX_INITIALIZER;
 pthread_cond_t cond1 = PTHREAD_COND_INITIALIZER;
 
+typedef struct
+{
+	int* array;
+	int length;
+} Shared;
+
 int main()
 {
-	int shared[] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+	Shared shared;
 	pthread_t thread1, thread2;
+	int i;
 
+	shared.array = malloc( 10 * sizeof(int) );
+	shared.length = 10;
+
+	for ( i = 0; i < shared.length; i++ )
+	{
+		shared.array[i] = i;
+	}
+
+	pthread_create( &thread1, NULL, &sharedRead, &shared );
+	pthread_create( &thread2, NULL, &sharedRead, &shared );
+
+	pthread_join( thread1, NULL );
+	pthread_join( thread2, NULL );
+	
 	pthread_create( &thread1, NULL, &shoop, NULL );
 	pthread_create( &thread2, NULL, &daWhoop, NULL );
 
@@ -23,56 +44,55 @@ int main()
 	return 0;
 }
 
-void* sharedRead( int* array, int length )
+void* sharedRead( void* in )
 {
 	int i;
+	Shared* shared = (Shared*)in;
 
-	for ( i = 0; i < length; i++ )
+	for ( i = 0; i < shared -> length; i++ )
 	{
-		printf( "Loop %d: %d\n", i, array[i] );
+		printf( "Loop %d: %d\n", i, shared -> array[i] );
 	}
+
+	return in;
 }
 
-void* shoop()
+void* shoop( void* in )
 {
 	int i;
 
 	for ( i = 0; i < 1000; i++ )
 	{
-		printf( "Shoop locking mutex\n" );
 		pthread_mutex_lock( &mutex1 );
 		if ( !shooping )
 		{
-			printf( "Shoop waiting for lock\n" );
 			pthread_cond_wait( &cond1, &mutex1 );
 		}
 		printf( "Shoop acquired lock : Shoop %d\n", i );
 		shooping = 0;
-		printf( "Shoop signaling condition\n" );
 		pthread_cond_signal( &cond1 );
-		printf( "Shoop unlocking mutex\n");
 		pthread_mutex_unlock( &mutex1 );
 	}
+
+	return 0;
 }
 
-void* daWhoop()
+void* daWhoop( void* in )
 {
 	int i;
 
 	for ( i = 0; i < 1000; i++ )
 	{
-		printf( "Da Whoop locking mutex\n" );
 		pthread_mutex_lock( &mutex1 );
 		if ( shooping )
 		{
-			printf( "Da Whoop waiting for lock\n" );
 			pthread_cond_wait( &cond1, &mutex1 );
 		}
 		printf( "Da Whoop acquire lock : Da Whoop %d\n", i );
 		shooping = 1;
-		printf( "Da Whoop signalling condition\n" );
 		pthread_cond_signal( &cond1 );
-		printf( "Da Whoop unlocking mutex\n" );
 		pthread_mutex_unlock( &mutex1 );
 	}
+
+	return 0;
 }
